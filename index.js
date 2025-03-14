58ff4c3070f78a3d4c2a133dd90ea43576745b78
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-14 18:29:39 +0300

    my first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const foo = function(){
+    return 'bar';
+}
+console.log(foo)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+console.log(add (2,3));
+
+const nums = [1,2,3];
+const squares = nums.map(x => x ** 2);
+console.log(squares);
+console.log(nums);
+
+
+
